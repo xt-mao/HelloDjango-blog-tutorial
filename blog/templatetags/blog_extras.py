8fe1aec9be4d83796f3eb9490b36071fb37b5092
xt-mao <cos_mopolitan@163.com>
2020-01-13 14:06:26 +0800

    number of posts in category and tag

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models.aggregates import Count
 
 from ..models import Post, Category, Tag
 
@@ -8,7 +9,7 @@ register = template.Library()
 @register.inclusion_tag('blog/inclusions/_recent_posts.html', takes_context=True)
 def show_recent_posts(context, num=5):
     return {
-        'recent_post_list': Post.objects.all().order_by('-created_time')[:num],
+        'recent_post_list': Post.objects.all()[:num],
     }
 
 
@@ -21,13 +22,15 @@ def show_archives(context):
 
 @register.inclusion_tag('blog/inclusions/_categories.html', takes_context=True)
 def show_categories(context):
+    category_list = Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
     return {
-        'category_list': Category.objects.all(),
+        'category_list': category_list,
     }
 
 
 @register.inclusion_tag('blog/inclusions/_tags.html', takes_context=True)
 def show_tags(context):
+    tag_list = Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
     return {
-        'tag_list': Tag.objects.all(),
+        'tag_list': tag_list,
     }
